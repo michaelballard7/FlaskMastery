051ec0baa37e717683577cc136ea77a650e2c9f5
Michael Anthony Ballard <MichaelBallard@Michaels-MacBook-Pro-2.local>
2019-04-24 18:05:38 -0400

    updated fetch

--- flask_fullstack/frontEnd/src/app.js
+++ flask_fullstack/frontEnd/src/app.js
@@ -1,9 +1,12 @@
 
 url = "http://127.0.0.1:5000/store"
 
+
 content = fetch(url).then(function(resp){
     return resp.json();
 })
 .then(function(myJson){
-    console.log(JSON.stringify(myJson));
+    var name = myJson.stores[0].name
+    console.log(name)
+
 });
