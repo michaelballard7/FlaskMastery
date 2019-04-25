647d4c3c1830a323adacec698c4d748838d9c902
Michael Anthony Ballard <MichaelBallard@Michaels-MacBook-Pro-2.local>
2019-04-25 00:33:38 -0400

    updated post endpoint

--- flaskRestFul/code/app.py
+++ flaskRestFul/code/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 # define the app and api
@@ -16,10 +16,31 @@ class Item(Resource):
             return {'item':None}, 404
 
     def post(self, name):
-        item = {'name':name,'price':12.00}
+
+        data = request.get_json(force=True) # recieve data from headers
+        item = {'name':data['name'],'price':data['price']}
         items.append(item)
         return item, 201
 
+
+    # def delete(self,name):
+    #     item = [item for item in items if item['name'] == name]
+    #     if item:
+    #         del items[name]
+    #         return items
+    #     else:
+    #         return {"error, object not for or deleted"}, 404
+
+class ItemList(Resource):
+    def get(self):
+        if items:
+            return {"items":items}, 201
+        else:
+            return {"items": None}, 404
+
+
 api.add_resource(Item, '/item/<string:name>')
 
-app.run()
+api.add_resource(ItemList, '/items')
+
+app.run(port=5000, debug=True)
