68c9fc7298a9fbe2092cc98cc5fb98b14491546e
Michael Anthony Ballard <MichaelBallard@Michaels-MBP-2.fios-router.home>
2019-04-26 01:05:44 -0400

    updated endpoints

--- flaskRestFul/code/app.py
+++ flaskRestFul/code/app.py
@@ -15,20 +15,21 @@ class Item(Resource):
         return {'item':item}, 200 if item else 404
 
     def post(self, name):
-        if item = next(filter(lambda x: x['name'] == name, items),None) is not None:
+        if next(filter(lambda x: x['name'] == name, items),None):
             return {f"message": "An item with the name {name} exists already"},400
-        data = request.get_json(force=True) # recieve data from headers
+
+        data = request.get_json() # recieve data from headers
         item = {'name':data['name'],'price':data['price']}
         items.append(item)
         return item, 201
 
-    def delete(self,name):
-        item = [item for item in items if item['name'] == name]
-        if item:
-            del items[name]
-            return items
-        else:
-            return {"error, object not for or deleted"}, 404
+    # def delete(self,name):
+    #     item = [item for item in items if item['name'] == name]
+    #     if item:
+    #         del items[name]
+    #         return items
+    #     else:
+    #         return {"error, object not for or deleted"}, 404
 
 class ItemList(Resource):
     def get(self):
@@ -39,7 +40,6 @@ class ItemList(Resource):
 
 
 api.add_resource(Item, '/item/<string:name>')
-
 api.add_resource(ItemList, '/items')
 
 app.run(port=5000, debug=True)
