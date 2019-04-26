38885d189b6fad3414fbe8aefecb5401747b3a4f
Michael Anthony Ballard <MichaelBallard@Michaels-MacBook-Pro-2.local>
2019-04-26 14:57:05 -0400

    added jwt library for auth

--- flaskRestFul/code/app.py
+++ flaskRestFul/code/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import  JWT
+from security import authenticate, identity
 from secrets import *
 
 # define the app and api
@@ -7,6 +9,7 @@ app = Flask(__name__)
 app.secret_key = SECRET_KEY
 api = Api(app)
 
+jwt = JWT(app,authenticate, identity) # creates /auth, send a user name and password if matches returns a token
 items = []
 
 class Item(Resource):
@@ -23,13 +26,13 @@ class Item(Resource):
         items.append(item)
         return item, 201
 
-    # def delete(self,name):
-    #     item = [item for item in items if item['name'] == name]
-    #     if item:
-    #         del items[name]
-    #         return items
-    #     else:
-    #         return {"error, object not for or deleted"}, 404
+    def delete(self,name):
+        item = [item for item in items if item['name'] == name]
+        if item:
+            del items[name]
+            return items
+        else:
+            return {"error, object not for or deleted"}, 404
 
 class ItemList(Resource):
     def get(self):
@@ -39,7 +42,7 @@ class ItemList(Resource):
             return {"items": None}, 404
 
 
-api.add_resource(Item, '/item/<string:name>')   
+api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 
 app.run(port=5000, debug=True)
